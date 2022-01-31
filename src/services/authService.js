511bfc106ec1dac74119be835f648924df5791eb
uttampatro <uttampatro890@gmail.com>
2022-01-31 17:12:37 +0530

    service

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,18 +2,25 @@ import * as config from '../config/api';
 import axios from './axios';
 
 const login = async ({ email, password }) => {
-    const response = await axios.post(`${config.apiConfig.baseUrl}/v1/login`, {
-        email,
-        password,
-    });
-    if (response.data) {
-        localStorage.setItem(
-            'accessToken',
-            JSON.stringify(response.data.accessToken)
+    try {
+        const response = await axios.post(
+            `${config.apiConfig.baseUrl}/v1/login`,
+            {
+                email,
+                password,
+            }
         );
+        if (response.data) {
+            localStorage.setItem(
+                'accessToken',
+                JSON.stringify(response.data.accessToken)
+            );
+        }
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-    console.log(response.data);
-    return response.data;
 };
 
 const logout = async () => {
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,2 +1,43 @@
 import * as config from '../config/api';
 import axios from './axios';
+
+const createUser = async ({ username, mobileNumber, email, address }) => {
+    try {
+        const response = await axios.post(
+            `${config.apiConfig.baseUrl}/v1/createUser`,
+            {
+                username,
+                mobileNumber,
+                email,
+                address,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getAllUser = async () => {
+    try {
+        const response = await axios.get(
+            `${config.apiConfig.baseUrl}/v1/getUserList`
+        );
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const deleteUser = async id => {
+    try {
+        const response = await axios.delete(
+            `${config.apiConfig.baseUrl}/v1/deleteUser/${id}`
+        );
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export { createUser, getAllUser, deleteUser };
