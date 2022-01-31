39c6d71b5bcdcda96567e4b4058214f3530243bb
uttampatro <uttampatro890@gmail.com>
2022-01-31 19:11:26 +0530

    netlify

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ const localApiConfig = {
     baseUrl: 'http://localhost:5000',
 };
 const prodApiConfig = {
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'https://assignment-05-service.herokuapp.com/',
 };
 console.log(process.env.REACT_APP_ENVIRONMENT);
 
